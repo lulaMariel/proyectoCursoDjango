b3b20c18a54891305d6aee056220d3af0ae217a1
lulaMariel <luciana.lavalle@gmail.com>
2024-12-12 10:04:49 -0300

    video 46 inclusive (no hago eso porque no puedo, email)

--- acontacto/views.py
+++ acontacto/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import FormularioContacto
 
 def contacto(request):
     formulario_contacto = FormularioContacto
+
+    if request.method == "POST":
+        formulario_contacto = FormularioContacto(data = request.POST)
+
+        if formulario_contacto.is_valid():
+            nombre = request.POST.get('nombre')
+            email = request.POST.get('email')
+            contenido = request.POST.get('contenido')
+
+            return redirect('/contacto/?valido')
 
     return render(request, "acontacto/contacto_view.html", {'mi_formulario': formulario_contacto})
